a66f42495861f00a88178c60302b1aac32892f3f
averagehuman <cyclebelfast@gmail.com>
2014-08-24 19:48:50 +0000

    rework nginx installation

---
+++ etc/nginx/install.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+set -e
+
+version=
+prefix=
+args=
+
+while [ $# -gt 0 ]; do
+
+    case $1 in
+
+	    --usage|--help)
+		cat - <<-EOF
+		Description: Download and install nginx
+		Example: $ ./install.sh --version=1.6.1 --prefix=/opt/nginx
+		EOF
+		exit 0
+	    ;;
+	    --version=*)
+		version=${1#--version=}
+	    ;;
+
+	    --prefix=*)
+		prefix=${1#--prefix=}
+	    ;;
+
+	    *)
+		args="$args $1"
+	    ;;
+
+    esac
+
+    shift
+
+done
+
+
+if [ ! $version ]; then echo "--version is required"; exit 1; fi
+if [ ! $prefix ]; then echo "--prefix is required"; exit 1; fi
+
+# remove trailing slash
+prefix="${prefix%/}"
+version="nginx-${version}"
+module_root="$(pwd)"
+
+
+archive="${version}.tar.gz"
+url="http://nginx.org/download/${archive}"
+
+rm -rf $version
+
+if [ ! -e $archive ]; then
+    wget -O $archive $url
+fi
+
+tar xzvf $archive
+
+apt-get -y install build-essential zlib1g zlib1g-dev libpcre3 libpcre3-dev
+apt-get -y install libgd3 libgeoip1 libgeoip-dev libssl1.0.0 libxml2 libxslt1.1
+
+pushd $version
+./configure --prefix="$prefix" --sbin-path="$prefix/bin/nginx" $args
+make build
+make install
+popd
+
+rm -rf $version
+rm -rf $archive
+
