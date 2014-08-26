a76fbcbb14d15d8815174cdee0eb40c425462628
averagehuman <cyclebelfast@gmail.com>
2014-08-26 06:57:38 +0000

    rework existing image checks

--- etc/docker/common.sh
+++ etc/docker/common.sh
@@ -4,12 +4,20 @@ quote() {
     sed 's/[]\.|$(){}?+*^]/\\&/g' <<< "$*"
 }
 
-exists() {
+image_exists() {
     if [ $(docker images | awk '{print $1":"$2}' | grep -x "$(quote $1)") ]; then
-        echo "y"
+        echo 1
     else
-        echo "n"
+        echo 0
     fi
+}
+
+fail_if_image_exists() {
+    if [ $(image_exists $1) -eq 1 ]; then
+        echo "Image '$1' exists. $2"
+        exit 1
+    fi
+    exit 0
 }
 
 remove_image() {
@@ -17,8 +25,9 @@ remove_image() {
     if [ $? -eq 0 ]; then
         docker ps -a | grep Exited | awk '{print $1}' | xargs docker rm
     fi
-    if [ "$(exists $1)" = "y" ]; then
-        docker rmi -f $1
+    image_exists "$1" >/dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        docker rmi -f "$1"
     fi
 }
 
